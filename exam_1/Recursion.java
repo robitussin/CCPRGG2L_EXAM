d6fff55d2507027498a3a03fc5221d9008368018
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-01-31 08:19:05 +0800

    Update Recursion.java

--- exam_1/Recursion.java
+++ exam_1/Recursion.java
@@ -1,7 +1,36 @@
-import java.util.Scanner;
+public class App {
 
-public class Recursion {
     public static void main(String[] args) {
 
+        // Instructions (Important!)
+
+        // Without using arrays or loops or regular expressions, apply recursion to remove specific letters in
+        // your full name.
+
+        // 1. If the last letter of your surname ends with a vowel, remove all vowels in
+        // your full name.
+
+        // 2. If the last letter of your surname ends with a consonant, remove all
+        // consonants in your full name.
+
+        // 3. Use only lowercase letters
+        
+        // 4. Each letter should be removed one by one.
+
+        // Example:
+        // Full name is "elizer ponio jr".
+        // The output should be "lzr pn jr".
+
+        // Put your complete name in the "fullName" variable.
+        // Example:
+        // String fullName = "elizer ponio jr";
+
+        String fullName = "";
+
+        removeVowel(fullName);
+    }
+
+    static void removeVowel(String str) {
+            // Add code here
     }
 }
